b6465cb47b27b379a0ad2f3c375dab212926bab1
LSV310 <anicet.gruet@gmail.com>
2025-05-14 15:21:07 +0200

    q_rsqrt fonctionnel (pour de vrai)

--- libft/srcs/maths/q_rsqrt.c
+++ libft/srcs/maths/q_rsqrt.c
@@ -6,7 +6,7 @@
 /*   By: agruet <agruet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 11:51:14 by agruet            #+#    #+#             */
-/*   Updated: 2025/05/11 14:43:45 by agruet           ###   ########.fr       */
+/*   Updated: 2025/05/14 15:18:51 by agruet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,14 @@
 
 double	q_rsqrt(double number)
 {
-	long	i;
-	double	x2;
-	double	y;
-	double	threehalfs;
+	long			i;
+	double			x2;
+	double			y;
+	const double	threehalfs = 1.5;
 
-	threehalfs = number * 1.5;
 	x2 = number * 0.5;
 	y = number;
-	i = *(long *) &y;
+	i = *(int64_t *) &y;
 	i = 0x5fe6eb50c7b537a9 - (i >> 1);
 	y = *(double *) &i;
 	y = y * (threehalfs - (x2 * y * y));
