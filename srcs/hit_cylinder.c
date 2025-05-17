3c22d0eaf1a3ae8335a4e9e3d6d564539483b45e
Biny17 <tristangallet17@gmail.com>
2025-05-17 04:49:12 +0200

    ça marchait, j'ai tout refait et maintenant ça marche plus hihi

--- srcs/hit_cylinder.c
+++ srcs/hit_cylinder.c
@@ -6,65 +6,80 @@
 /*   By: tgallet <tgallet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/14 22:38:23 by tgallet           #+#    #+#             */
-/*   Updated: 2025/05/14 22:46:51 by tgallet          ###   ########.fr       */
+/*   Updated: 2025/05/17 04:46:33 by tgallet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
 
-bool intersect_cylinder_body(t_cylinder *cylinder, t_ray *r, t_hit *hit)
+static bool	intersect_cyl_body_two(
+	t_cylinder *cyl, t_ray *r, t_hit *hit, t_vec hit_point)
 {
-	const t_vec		oc = vsub(r->p, cylinder->pos);
-	const double	a = dot(r->dir, r->dir) - pow(dot(r->dir, cylinder->axis), 2);
-	const double	b = 2 * (dot(r->dir, oc) - dot(r->dir, cylinder->axis) * dot(oc, cylinder->axis));
-	const double	c = dot(oc, oc) - pow(dot(oc, cylinder->axis), 2) - pow(cylinder->radius, 2);
+	double	height;
+
+	height = dot(vsub(hit_point, cyl->pos), cyl->axis);
+	if (height < -cyl->height / 2 || height > cyl->height / 2)
+		return (false);
+	hit->p = hit_point;
+	hit->normal = norm(vsub(hit_point, vadd(cyl->pos, vmul(cyl->axis, height))));
+	hit->front = (dot(r->dir, hit->normal) < 0);
+	hit->mat = &cyl->mat;
+	hit->u = atan2(hit->normal.z, hit->normal.x) / (2 * PI);
+	hit->v = (height + cyl->height / 2) / cyl->height;
+	return (true);
+}
+
+bool intersect_cyl_body(t_cylinder *cyl, t_ray *r, t_hit *hit)
+{
+	const t_vec		oc = vsub(r->p, cyl->pos);
+	const double	a = dot(r->dir, r->dir) - pow(dot(r->dir, cyl->axis), 2);
+	const double	b = 2 * (dot(r->dir, oc)
+						- dot(r->dir, cyl->axis) * dot(oc, cyl->axis));
+	const double	c = dot(oc, oc)
+						- pow(dot(oc, cyl->axis), 2) - pow(cyl->radius, 2);
 	const double	delta = b * b - 4 * a * c;
 	double 			t;
-	double			height_proj;
+	t_vec			hit_point;
 
 	if (delta < 0)
 		return (false);
 	t = (-b - sqrt(delta)) / (2 * a);
 	if (t < 0 || hit->t < t)
 		return (false);
-	hit->t = t;
-	hit->p = vadd(r->p, vmul(r->dir, hit->t));
-	height_proj = dot(vsub(hit->p, cylinder->pos), cylinder->axis);
-	if (height_proj < -cylinder->height / 2 || height_proj > cylinder->height / 2)
+	hit_point = vadd(r->p, vmul(r->dir, hit->t));
+	if (!intersect_cyl_body_two(cyl, r, hit, hit_point))
 		return (false);
+	hit->t = t;
 	return (true);
 }
 
-bool intersect_cylinder_caps(t_cylinder *cylinder, t_ray *r, t_hit *hit)
+bool intersect_cyl_caps(
+	t_cylinder *cylinder, t_ray *r, t_hit *hit, t_vec cap_center)
 {
-	return (false);
-}
+	const double	denom = dot(cylinder->axis, r->dir);
+	double			t;
 
-void set_hit_info(t_cylinder *cylinder, t_ray *r, t_hit *hit)
-{
-	t_vec	hit_to_center = vsub(hit->p, cylinder->pos);
-	t_vec	proj_axis = vmul(cylinder->axis, dot(hit_to_center, cylinder->axis));
-	double	height_proj;
-
-	hit->normal = norm(vsub(hit_to_center, proj_axis));
-	hit->front = (dot(r->dir, hit->normal) < 0);
+	if (fabs(denom) < 0.000001)
+		return (false);
+	t = dot(vsub(cap_center, r->p), cylinder->axis) / denom;
+	if (t < 0 || hit->t < t)
+		return (false);
+	hit->p = vadd(r->p, vmul(r->dir, t));
+	hit->t = t;
+	hit->normal = cylinder->axis;
+	hit->front = (denom < 0);
 	hit->mat = &cylinder->mat;
-	hit->u = atan2(hit->normal.z, hit->normal.x) / (2 * PI);
-	height_proj = dot(vsub(hit->p, cylinder->pos), cylinder->axis);
-	hit->v = (height_proj + cylinder->height / 2) / cylinder->height;
+	hit->u = fmod(hit->p.x - cap_center.x, 4);
+	hit->v = fmod(hit->p.y - cap_center.y, 4);
+	return (true);
 }
 
 bool hit_cylinder(t_cylinder *cylinder, t_ray *r, t_hit *hit)
 {
-	if (intersect_cylinder_body(cylinder, r, hit))
-	{
-		set_hit_info(cylinder, r, hit);
+	if (intersect_cyl_body(cylinder, r, hit)
+		|| intersect_cyl_caps(cylinder, r, hit,
+			vadd(cylinder->pos, vmul(cylinder->axis, cylinder->height / 2)))
+		|| intersect_cyl_caps(cylinder, r, hit,
+			vsub(cylinder->pos, vmul(cylinder->axis, cylinder->height / 2))))
 		return (true);
-	}
-	if (intersect_cylinder_caps(cylinder, r, hit))
-	{
-		set_hit_info(cylinder, r, hit);
-		return (true);
-	}
-	return (false);
 }
