d3c6a559884749d594292343c4c03b8607bcee49
Biny17 <tristangallet17@gmail.com>
2025-05-28 15:29:53 +0200

    background plus stylé

--- srcs/closest_hit.c
+++ srcs/closest_hit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   closest_hit.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: agruet <agruet@student.42.fr>              +#+  +:+       +#+        */
+/*   By: tgallet <tgallet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/13 02:56:07 by tgallet           #+#    #+#             */
-/*   Updated: 2025/05/21 11:28:40 by agruet           ###   ########.fr       */
+/*   Updated: 2025/05/28 15:29:23 by tgallet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,21 @@
 
 int32_t	background_color(t_ray *r)
 {
-	const t_vec	sky_top = (t_color){0.5, 0.65, 0.84};
-	const t_vec	sky_bot = (t_color){0.63, 0.72, 0.90};
+	const t_vec	sky_bot = (t_color){0.42, 0.55, 0.84};
+	const t_vec	sky_top = (t_color){0.7, 0.8, 1};
 
-	if (r->dir.y < 0)
-		return (vec_to_col(sky_top));
-	else
+	if (r->dir.y * r->dir.y < 0.02 * 0.02)
+		return (vec_to_col(
+			lerp_vec(
+				sky_bot,
+				sky_top,
+				(r->dir.y + 0.02) / 0.04
+			)
+		));
+	else if (r->dir.y < 0)
 		return (vec_to_col(sky_bot));
+	else
+		return (vec_to_col(sky_top));
 }
 
 int32_t	ray_to_color(t_ray *r, t_elem_lst *elems, size_t frame)
