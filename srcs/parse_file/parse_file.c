e4836ea916b12c01146a40fc9af45910bf25075b
LSV310 <anicet.gruet@gmail.com>
2025-09-15 18:29:08 +0200

    added comments option in rt files

--- srcs/parse_file/parse_file.c
+++ srcs/parse_file/parse_file.c
@@ -6,7 +6,7 @@
 /*   By: agruet <agruet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 11:21:35 by agruet            #+#    #+#             */
-/*   Updated: 2025/09/09 18:46:48 by agruet           ###   ########.fr       */
+/*   Updated: 2025/09/11 14:58:48 by agruet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,7 +75,7 @@ bool	read_rtfile(int fd, t_elem_lst *elements, t_arena *arena)
 		free(line);
 		if (!split)
 			return (print_err("Memory allocation failed", 0));
-		if (split[0] && split[0][0] && split[0][0] != '\n')
+		if (split[0] && split[0][0] && split[0][0] != '#' && split[0][0] != '\n')
 		{
 			if (parse_elements(elements, split, i) == false)
 				return (false);
