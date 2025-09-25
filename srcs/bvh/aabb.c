a95bc9736bb32afe9b35b17796121695a7ff5c46
LSV310 <anicet.gruet@gmail.com>
2025-09-25 18:02:13 +0200

    fix cyl aabb

--- srcs/bvh/aabb.c
+++ srcs/bvh/aabb.c
@@ -6,7 +6,7 @@
 /*   By: agruet <agruet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/06 12:58:38 by agruet            #+#    #+#             */
-/*   Updated: 2025/09/08 17:22:51 by agruet           ###   ########.fr       */
+/*   Updated: 2025/09/25 17:57:26 by agruet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,16 +29,24 @@ static t_aabb	get_sphere_aabb(t_sphere *sphere)
 		.max = {{pos.x + radius, pos.y + radius, pos.z + radius}}});
 }
 
-static t_aabb	get_cylinder_aabb(t_cylinder *cylinder)
+static t_aabb	get_cylinder_aabb(t_cylinder *cyl)
 {
-	const t_point	a = cylinder->pos;
-	const t_point	b = vadd(a, vmul(cylinder->axis, cylinder->height));
-	const t_point	tmp = {{cylinder->radius, cylinder->radius,
-		cylinder->radius}};
+	t_aabb			box;
+	const t_vec		half_axis = vmul(cyl->axis, cyl->height * 0.5f);
+	const t_point	base1 = vsub(cyl->pos, half_axis);
+	const t_point	base2 = vadd(cyl->pos, half_axis);
+	t_vec			ext;
 
-	return ((t_aabb){.min = vsub(vsub(vmin(a, b), tmp),
-			vmul(cylinder->axis, cylinder->height / 2)),
-		.max = vadd(vmax(a, b), tmp)});
+	ext.x = cyl->radius * sqrtf(1.0f - cyl->axis.x * cyl->axis.x);
+	ext.y = cyl->radius * sqrtf(1.0f - cyl->axis.y * cyl->axis.y);
+	ext.z = cyl->radius * sqrtf(1.0f - cyl->axis.z * cyl->axis.z);
+	box.min.x = fminf(base1.x, base2.x) - ext.x;
+	box.max.x = fmaxf(base1.x, base2.x) + ext.x;
+	box.min.y = fminf(base1.y, base2.y) - ext.y;
+	box.max.y = fmaxf(base1.y, base2.y) + ext.y;
+	box.min.z = fminf(base1.z, base2.z) - ext.z;
+	box.max.z = fmaxf(base1.z, base2.z) + ext.z;
+	return (box);
 }
 
 t_aabb	get_elem_aabb(void *elem)
