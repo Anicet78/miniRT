18e65609a2eb28f017fa941292894350da37d8d8
LSV310 <anicet.gruet@gmail.com>
2025-09-26 18:53:51 +0200

    added cone aabb

--- srcs/bvh/aabb.c
+++ srcs/bvh/aabb.c
@@ -6,7 +6,7 @@
 /*   By: agruet <agruet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/06 12:58:38 by agruet            #+#    #+#             */
-/*   Updated: 2025/09/25 17:57:26 by agruet           ###   ########.fr       */
+/*   Updated: 2025/09/26 18:13:10 by agruet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ static t_aabb	get_sphere_aabb(t_sphere *sphere)
 
 static t_aabb	get_cylinder_aabb(t_cylinder *cyl)
 {
-	t_aabb			box;
+	t_aabb			aabb;
 	const t_vec		half_axis = vmul(cyl->axis, cyl->height * 0.5f);
 	const t_point	base1 = vsub(cyl->pos, half_axis);
 	const t_point	base2 = vadd(cyl->pos, half_axis);
@@ -40,13 +40,32 @@ static t_aabb	get_cylinder_aabb(t_cylinder *cyl)
 	ext.x = cyl->radius * sqrtf(1.0f - cyl->axis.x * cyl->axis.x);
 	ext.y = cyl->radius * sqrtf(1.0f - cyl->axis.y * cyl->axis.y);
 	ext.z = cyl->radius * sqrtf(1.0f - cyl->axis.z * cyl->axis.z);
-	box.min.x = fminf(base1.x, base2.x) - ext.x;
-	box.max.x = fmaxf(base1.x, base2.x) + ext.x;
-	box.min.y = fminf(base1.y, base2.y) - ext.y;
-	box.max.y = fmaxf(base1.y, base2.y) + ext.y;
-	box.min.z = fminf(base1.z, base2.z) - ext.z;
-	box.max.z = fmaxf(base1.z, base2.z) + ext.z;
-	return (box);
+	aabb.min.x = fminf(base1.x, base2.x) - ext.x;
+	aabb.max.x = fmaxf(base1.x, base2.x) + ext.x;
+	aabb.min.y = fminf(base1.y, base2.y) - ext.y;
+	aabb.max.y = fmaxf(base1.y, base2.y) + ext.y;
+	aabb.min.z = fminf(base1.z, base2.z) - ext.z;
+	aabb.max.z = fmaxf(base1.z, base2.z) + ext.z;
+	return (aabb);
+}
+
+static t_aabb get_cone_aabb(t_cone *cone)
+{
+	t_aabb			aabb;
+	const t_vec		axis = norm(cone->axis);
+	const t_point	base = vadd(cone->pos, vmul(axis, cone->height));
+	t_vec			d;
+
+	d.x = cone->radius * sqrtf(1.0f - axis.x * axis.x);
+	d.y = cone->radius * sqrtf(1.0f - axis.y * axis.y);
+	d.z = cone->radius * sqrtf(1.0f - axis.z * axis.z);
+	aabb.min.x = fminf(cone->pos.x, base.x - d.x);
+	aabb.max.x = fmaxf(cone->pos.x, base.x + d.x);
+	aabb.min.y = fminf(cone->pos.y, base.y - d.y);
+	aabb.max.y = fmaxf(cone->pos.y, base.y + d.y);
+	aabb.min.z = fminf(cone->pos.z, base.z - d.z);
+	aabb.max.z = fmaxf(cone->pos.z, base.z + d.z);
+	return (aabb);
 }
 
 t_aabb	get_elem_aabb(void *elem)
@@ -59,6 +78,6 @@ t_aabb	get_elem_aabb(void *elem)
 	else if (type == CYLINDER)
 		return (get_cylinder_aabb((t_cylinder *)elem));
 	else if (type == CONE)
-		return (get_cylinder_aabb((t_cylinder *)elem));
+		return (get_cone_aabb((t_cone *)elem));
 	return ((t_aabb){{{0.0, 0.0, 0.0}}, {{0.0, 0.0, 0.0}}});
 }
